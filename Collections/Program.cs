c9bf29ace4987993fc81674bce7e4d28ced8c0e9
Zeynep Mirzooğlu <zeynepmirzooglu@gmail.com>
2022-08-29 00:18:59 +0300

    GenericType

---
+++ Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] {"Zeynep","Gani","Muhammed","Duygu"};
+
+            List<string> names2 = new List<string>
+            {
+                "Zeynep","Gani","Muhammed","Duygu"
+            };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("Ali");
+            Console.WriteLine(names2[4]);
+        }
+    }
+}
+
---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    public class MyList<T>
+    {
+         T[] items;
+
+         public MyList()
+         {
+             items = new T[0];
+         }
+        public void Add(T item)
+        {
+            //Dizinin elemanlarını bir arttır.
+            //Referans numarası eleman sayısı artınca kaybolmasın diye geçici bir diziye atayıp newlenince diğer referanslar silinmesin diye.
+
+            T[] tempArray = items;
+            items=new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                //Geçici diziden elemanları geri almak için yazılır.
+                items[i] = tempArray[i];
+            }
+            //eklenmek istenen elemanı eklemek için:
+
+            items[items.Length-1] = item;
+        }
+    }
+}
